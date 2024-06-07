828a5bcb8b6f548b458cb80051d6256a04451858
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2024-06-07 15:53:44 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'https://luminous-frangollo-2efcc0.netlify.app' // Allow requests from this origin
+    origin: 'https://whimsical-custard-923773.netlify.app' // Allow requests from this origin
   }));
 app.use(express.json());
 
