e412724482056563c0f0501776056c261d4acc9a
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2024-06-09 19:39:49 -0400

    Update addsession.js

--- server/routes/addsession.js
+++ server/routes/addsession.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import mongoose from 'mongoose'; // Import mongoose
 import authMiddleware from '../middleware/authMiddleware.js';
 import Session from '../models/Session.js';
+
 
 const router = express.Router();
 
@@ -24,25 +24,10 @@ router.post('/addsession', async (req, res) => {
             user: userId // Associate the session with the logged-in user
         });
         
-        // Save session to database
+        //save session to database
         await session.save();
         res.status(201).json({ success: true, data: session });
         
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, error: 'Server Error' });
-    }
-});
-
-// Retrieve sessions route
-router.get('/sessions', async (req, res) => {
-    try {
-        const userId = req.user.id;
-
-        // Retrieve sessions associated with the logged-in user
-        const sessions = await Session.find({ user: userId });
-
-        res.status(200).json({ success: true, data: sessions });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, error: 'Server Error' });
