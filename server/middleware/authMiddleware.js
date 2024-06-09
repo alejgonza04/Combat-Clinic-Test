a4ab74785e67f1ef06df62c3b9f8f3890c61648b
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2024-06-08 21:16:39 -0400

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,32 +4,32 @@ import User from '../models/User.js';
 const authMiddleware = async (req, res, next) => {
 
     // Get the token from the request headers
-const token = req.header('Authorization');
-
-// Check if token is present
-if (!token) {
-  return res.status(401).json({ message: 'Authorization denied. No token provided.' });
-}
-
-try {
-  // Verify the token
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-  // Find the user associated with the token
-  const user = await User.findById(decoded.user.id).select('-password');
-
-  // Check if user exists
-  if (!user) {
-    return res.status(401).json({ message: 'Authorization denied. Invalid token.' });
-  }
-
-  // Attach the user object to the request
-  req.user = user;
-  next(); // Call the next middleware
-} catch (err) {
-  console.error(err.message);
-  return res.status(500).json({ message: 'Server Error' });
-}
+    const token = req.header('Authorization');
+    
+    // Check if token is present
+    if (!token) {
+      return res.status(401).json({ message: 'Authorization denied. No token provided.' });
+    }
+    
+    try {
+      // Verify the token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+      // Find the user associated with the token
+      const user = await User.findById(decoded.user.id).select('-password');
+    
+      // Check if user exists
+      if (!user) {
+        return res.status(401).json({ message: 'Authorization denied. Invalid token.' });
+      }
+    
+      // Attach the user object to the request
+      req.user = user;
+      next(); // Call the next middleware
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({ message: 'Server Error' });
+    }
 };
 
 export default authMiddleware;
