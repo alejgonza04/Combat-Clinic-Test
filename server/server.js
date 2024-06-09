fb301aa058797df1e1ca71f7547929261aba8002
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2024-06-08 20:52:49 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.js';
+import addSessionRoutes from '../routes/addSession.js';
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authRoutes);
+app.use('/addsession', addSessionRoutes);
 
 const startServer = async () => {
     try {
