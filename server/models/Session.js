f73e3050002de0710365281c684008656f57ed5f
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2024-06-09 22:03:43 -0400

    Update Session.js

--- server/models/Session.js
+++ server/models/Session.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
+
 const sessionSchema = new mongoose.Schema({
     sessionType: String,
     sessionLength: String,
     sparringTime: String,
     techniques: String,
     date: String,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 const Session = mongoose.model('Session', sessionSchema);
