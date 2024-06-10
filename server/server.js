f571b9ef7934b52c742adb843968c214f2e6db3e
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2024-06-10 16:11:48 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.js';
-import addSessionRoutes from './routes/addsession.js';
+import sessionRoutes from './routes/session.js';
 
 dotenv.config();
 
 const app = express();
+
 app.use(cors({
-    origin: 'https://verdant-fairy-45e354.netlify.app' // Allow requests from this origin
-  }));
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 
 connectDB();
@@ -20,24 +21,14 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authRoutes);
-app.use('/addsession', addSessionRoutes);
+app.use('/sessions', sessionRoutes);
 
 const startServer = async () => {
     try {
-
         app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
     } catch (error) {
         console.log(error);
-        if (error.name === 'ValidationError') {
-            return res.status(400).json({ success: false, error: 'Invalid session data' });
-        } else if (error.name === 'MongoError' && error.code === 11000) {
-            return res.status(400).json({ success: false, error: 'Duplicate session data' });
-        } else {
-            return res.status(500).json({ success: false, error: 'Server Error' });
-        }
     }
-}
-
-//app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
+};
 
 startServer();
