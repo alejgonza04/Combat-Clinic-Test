2783ced0c858623e94c6b18d9f789a6d35b25a54
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2024-06-10 16:10:28 -0400

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,30 +2,21 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
-const authMiddleware = async (req, res, next) => {
-    const authHeader = req.header('Authorization');
-  
-    if (!authHeader) {
-      return res.status(401).json({ message: 'Authorization denied. No token provided.' });
-    }
-  
-    const token = authHeader.split(' ')[1]; // Extract the token
-  
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const user = await User.findById(decoded.user.id).select('-password');
-  
-      if (!user) {
-        return res.status(401).json({ message: 'Authorization denied. Invalid token.' });
-      }
-  
-      req.user = user;
-      next();
-    } catch (err) {
-      console.error(err.message);
-      return res.status(500).json({ message: 'Server Error' });
-    }
-  };
-  
+const auth = async (req, res, next) => {
+    const token = req.header('x-auth-token');
 
-export default authMiddleware;
+    if (!token) {
+        return res.status(401).json({ message: 'No token, authorization denied' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Token is not valid' });
+    }
+};
+
+export default auth;
+
