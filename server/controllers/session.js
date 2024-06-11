9e7fc004cb792813e7d8de608a91bf8d9b17c98b
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2024-06-10 23:12:12 -0400

    Add files via upload

---
+++ server/controllers/session.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import SessionMessage from '../models/sessionMessage.js';
+
+export const getSessions = async (req, res) => { 
+    try {
+        const sessionMessages = await SessionMessage.find();
+                
+        res.status(200).json(sessionMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getSession = async (req, res) => { 
+    const { id } = req.params;
+
+    try {
+        const session = await SessionMessage.findById(id);
+        
+        res.status(200).json(session);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createSession = async (req, res) => {
+    const { sessionType, sessionLength, sparringTime, techniques, date } = req.body;
+
+    const newSessionMessage = new SessionMessage({ sessionType, sessionLength, sparringTime, techniques, date })
+
+    try {
+        await newSessionMessage.save();
+
+        res.status(201).json(newSessionMessage);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
---
+++ server/controllers/user.js
@@ -0,0 +1,37 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/user.js';
+
+const SECRET_KEY = process.env.JWT_SECRET || 'your_secret_key'; // use environment variable for the secret key
+
+export const signin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if(!existingUser) return res.status(404).json({ message: "User doesn't exist" });
+
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password); 
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id}, SECRET_KEY, { expiresIn: "1h" });
+        res.status(200).json({ result: existingUser, token });
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong.' });
+    }
+}
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if(existingUser) return res.status(400).json({ message: "User already exists" });
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const result = await User.create({ fullName, email, password: hashedPassword });
+
+        const token = jwt.sign({ email: result.email, id: result._id}, SECRET_KEY, { expiresIn: "1h" });
+        res.status(200).json({ result: result, token });
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong.' });
+    }
+}
