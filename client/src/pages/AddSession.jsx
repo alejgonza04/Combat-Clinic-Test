904526b04293d4bb14fb634642305a24b850d566
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2024-06-11 15:27:32 -0400

    Add files via upload

---
+++ client/src/pages/AddSession.jsx
@@ -0,0 +1,441 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import bjjImage from "../utils/images/jiujitsu.png";
+import thaiImage from "../utils/images/muaythai.png";
+import boxingImage from "../utils/images/boxing.png";
+import wrestlingImage from "../utils/images/wrestling.png";
+import DatePicker from "react-datepicker"; 
+import useClickedItem from "../components/useClickedItem.jsx";
+import "react-datepicker/dist/react-datepicker.css";
+import { useSession } from '../components/SessionContext.jsx';
+
+const Container = styled.div`
+height: 100%;
+flex: 1;
+display: flex;
+justify-content: center;
+padding: 22px 0px;
+align-items: flex-start;
+overflow-y: scroll;
+`;
+
+const Card = styled.div`
+height: auto;
+width: 1000px;
+padding: 24px;
+border: 1px solid ${({ theme }) => theme.black + 20};
+border-radius: 20px;
+background: linear-gradient(to bottom, rgba(0,0,0,0.9), rgba(0,0,0,0.80));
+box-shadow: 1px 6px 20px 0px ${({ theme }) => theme.black + 15};
+@media (max-width: 768px) {
+  width: 670px;
+  height: auto;
+  border-radius: 20px;
+}
+@media (max-width: 450px) {
+  border-radius: 10px;
+  width: 290px;
+  height: auto;
+  border-radius: 20px;
+  flex-direction: column;
+  justify-content: center;
+}
+`;
+
+const Button = styled.div`
+height: 10px;
+width: 600px;
+padding: 30px;
+display: flex; 
+justify-content: center; 
+align-items: center; 
+border: 1px solid ${({ theme }) => theme.red};
+border-radius: 20px;
+cursor: pointer;
+background: ${({ isHovered, theme }) => (isHovered ? theme.red :  theme.black )};
+box-shadow: 1px 6px 20px 0px ${({ theme }) => theme.black + 15};
+transition: background 0.3s ease;
+@media (max-width: 768px) {
+  border-radius: 10px;
+  width: 580px;
+  height: 10px;
+  border-radius: 20px;
+}
+@media (max-width: 450px) {
+  border-radius: 10px;
+  width: 200px;
+  height: 5px;
+  border-radius: 20px;
+}
+`;
+
+const ButtonContainer = styled.div`
+font-weight: 800;
+color: white;
+padding: 30px;
+font-size: 35px;
+flex-direction: column;
+align-items: center;
+gap: 10px; 
+display: flex;
+`;
+
+const ButtonText = styled.div`
+font-weight: 800;
+color: white;
+font-size: 35px;
+padding: 20px;
+align-items: center;
+justify-content: center;
+gap: 10px; 
+cursor: pointer;
+display: flex;
+@media (max-width: 768px) {
+  font-size: 23px; 
+  padding: 7px;
+}
+@media (max-width: 450px) {
+  font-size: 20px;
+}
+`;
+
+const CardContainer = styled.div`
+display: flex;
+align-items: flex-start;
+justify-content: space-between;
+@media (max-width: 450px) {
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+`;
+
+const CardContainer2 = styled.div`
+display: flex;
+align-items: flex-start;
+justify-content: center;
+gap: 185px;
+padding: 10px;
+flex-direction: column;
+`;
+
+const Text = styled.div`
+font-weight: 800;
+color: white;
+font-size: 35px;
+padding: 10px;
+flex-direction: column;
+align-items: center;
+gap: 10px; 
+display: flex;
+@media (max-width: 768px) {
+  font-size: 23px; 
+}
+@media (max-width: 450px) {
+  font-size: 20px;
+}
+`;
+
+const MobileText = styled.div`
+font-weight: 800;
+color: white;
+font-size: 35px;
+padding: 10px;
+flex-direction: column;
+align-items: center;
+gap: 10px; 
+display: flex;
+@media (max-width: 768px) {
+  font-size: 23px; 
+}
+@media (max-width: 450px) {
+  font-size: 13px;
+  flex-direction: row;
+}
+`;
+
+const Text2 = styled.div`
+font-weight: 800;
+color: white;
+font-size: 20px;
+padding: 10px;
+gap: 20px; 
+display: flex;
+@media (max-width: 768px) {
+  font-size: 20px; 
+}
+@media (max-width: 450px) {
+  font-size: 13px;
+  flex-direction: row;  /* Set flex direction to row */
+}
+`;
+
+const Text3 = styled.div`
+font-weight: 800;
+color: white;
+font-size: 35px;
+padding: 20px;
+flex-direction: column;
+align-items: center;
+gap: 10px; 
+display: flex;
+@media (max-width: 768px) {
+  font-size: 23px; 
+  padding: 7px;
+}
+@media (max-width: 450px) {
+  font-size: 20px;
+}
+`;
+
+const TechLink = styled.div`
+font-weight: 800;
+color: ${({ isClicked, theme }) => (isClicked ? theme.red : theme.white)};
+font-size: 30px; 
+transition: all 0.3s ease;
+cursor: pointer;
+transition: all 1s slide-in;
+text-decoration: none;
+&:hover {
+color: ${({ theme }) => theme.red};
+}
+@media (max-width: 768px) {
+  font-size: 20px; 
+}
+@media (max-width: 450px) {
+  font-size: 20px;
+}
+`;
+
+const Image = styled.img`
+width: 80px;
+cursor: pointer;
+display: flex;
+transition: opacity 0.3s ease; // Smooth transition for the opacity effect
+opacity: ${({ isClicked }) => (isClicked ? 0.3 : 1)}; 
+&:hover {
+  opacity: 0.3; // Set the desired opacity on hover
+}
+@media (max-width: 768px) {
+  width: 60px;
+}
+@media (max-width: 450px) {
+  width: 55px;
+}
+`;
+
+const TextBox = styled.input`
+width: 100%;
+height: 100%; 
+padding: 10px;
+font-size: 18px;
+border: 1px solid #ccc;
+border-radius: 4px;
+margin-top: 10px;
+position: relative;
+&::placeholder {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+}
+@media (max-width: 450px) {
+  width: 77%;
+  height: 50%; 
+  padding: 10px;
+  font-size: 13px;
+}
+`;
+
+const TextBox2 = styled.input`
+width: 50%;
+height: 50%; 
+padding: 10px;
+font-size: 18px;
+border: 1px solid #ccc;
+border-radius: 4px;
+margin-top: 10px;
+&::placeholder {
+  position: absolute;
+  top: 10px; 
+  left: 10px; 
+}
+@media (max-width: 450px) {
+  width: 97%;
+  height: 50%; 
+  padding: 10px;
+  font-size: 13px;
+}
+`;
+
+const StyledDatePicker = styled(DatePicker)`
+width: 200px; /* Adjust the width as needed */
+height: 40px; /* Adjust the height as needed */
+margin-top: 10px;
+@media (max-width: 768px) {
+  width: 200px;
+  height: 40px;
+}
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none; /* Remove underline */
+`;
+
+async function createSession(sessionData) {
+  try {
+    const response = await fetch('http://localhost:8080/addsession', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ...sessionData }), // Include the user's email in the request body
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to add session');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Add session error:', error.message);
+    throw error;
+  }
+}
+
+
+const AddSession = () => {
+    const [clickedLink, setClickedLink] = useState(null);
+    const [clickedImage, setClickedImage] = useState(null);
+    const [techTextBoxValue, techSetTextBoxValue] = useState("");
+    const [timeTextBoxValue, timeSetTextBoxValue] = useState("");
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [isHovered, setIsHovered] = useState(false);
+    const [selectedItem, handleItemClick] = useClickedItem();
+    const [item, setItem] = useState(new Map());
+    console.log("Initial item:", item);
+    const navigate = useNavigate();
+    const { addSession } = useSession();
+
+
+    const handleAddItem = async () => {
+      if (clickedLink && clickedImage && techTextBoxValue && timeTextBoxValue && selectedDate) {
+        // Combine all selected values into an object or array
+        const sessionData = {
+          sessionType: clickedImage,
+          sessionLength: clickedLink,
+          sparringTime: timeTextBoxValue + " min",
+          techniques: techTextBoxValue,
+          date: selectedDate
+        };
+          createSession(sessionData);
+          addSession(sessionData);
+          navigate('/sessions');
+
+
+          // Update the item Map with the new item
+          setItem(prevItem => {
+            const updatedItem = new Map([...prevItem, [prevItem.size, sessionData]]);
+            console.log("Updated item:", updatedItem);
+            return updatedItem;
+          });
+       
+      } else {
+        console.log("Please fill in all fields before adding the session.");
+      }
+  
+    };
+    
+    const allFieldsFilled = clickedLink && clickedImage && techTextBoxValue && timeTextBoxValue && selectedDate;
+
+    const handleTimeChange = (e) => {
+      const value = e.target.value;
+      // Ensure the value is numeric
+      if (/^\d*$/.test(value)) {
+        timeSetTextBoxValue(value);
+      }
+    };
+    
+    
+  return (
+    <Container>
+        <Card>
+            <CardContainer>
+            
+            <Text>
+                Session Type
+            <MobileText>
+                <Text2>
+                <Image src={bjjImage} isClicked={clickedImage === "BJJ"} onClick={() => {setClickedImage("BJJ"); }}/>
+                <Image src={thaiImage} isClicked={clickedImage === "Muay Thai"} onClick={() => {setClickedImage("Muay Thai"); }}/>
+                </Text2>
+                <Text2>
+                <Image src={boxingImage} isClicked={clickedImage === "Boxing"} onClick={() => {setClickedImage("Boxing"); }}/>
+                <Image src={wrestlingImage} isClicked={clickedImage === "Wrestling"} onClick={() => {setClickedImage("Wrestling");}}/>
+                </Text2>
+            </MobileText>
+            </Text>
+            <Text>
+                Session Length
+                <Text2>
+                <TechLink isClicked={clickedLink === "1h"} onClick={() => {setClickedLink("1h")}}> 1h </TechLink>
+                <TechLink isClicked={clickedLink === "2h"} onClick={() => {setClickedLink("2h")}}> 2h </TechLink>
+                <TechLink isClicked={clickedLink === "3h"} onClick={() => {setClickedLink("3h")}}> 3h </TechLink>
+                <TechLink isClicked={clickedLink === "4h"} onClick={() => {setClickedLink("4h")}}> 4h </TechLink>
+                </Text2>
+
+                <CardContainer2>
+                <Text>
+                    Sparring Time
+                    <TextBox2 
+                    type="text"
+                    value={timeTextBoxValue}
+                    onChange = {handleTimeChange}
+                    placeholder="Min"
+                    />
+                </Text>
+            </CardContainer2>
+            </Text>
+            <Text>
+               Choose Techniques 
+                <TextBox
+                type="text"
+                value={techTextBoxValue}
+                onChange={(e) => techSetTextBoxValue(e.target.value)}
+                placeholder="Enter Techniques Here"
+                />
+
+                <CardContainer2>
+                <Text3> 
+                    Date
+                    <StyledDatePicker
+                    selected={selectedDate}
+                    onChange={(date) => setSelectedDate(date)}
+                    dateFormat = "MMMM d, yyyy"
+                    />
+                </Text3>
+            </CardContainer2>
+            </Text>
+            </CardContainer>
+            <ButtonContainer>
+            <StyledLink to={allFieldsFilled ? '/' : '#'}>
+            <Button 
+              isHovered={isHovered} // Ensure the prop is passed correctly
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+              onClick={handleAddItem}
+            >
+              <ButtonText>
+                Add Session
+              </ButtonText>
+            </Button>
+            </StyledLink>
+            </ButtonContainer>
+        </Card>
+    </Container>
+  )
+}
+
+export default AddSession
+
---
+++ client/src/pages/Wrestling.jsx
@@ -0,0 +1,141 @@
+import styled from "styled-components";
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+
+
+const Container = styled.div`
+height: 100%;
+flex: 1;
+display: flex;
+justify-content: center;
+align-items: flex-start;
+padding: 22px 0px;
+overflow-y: scroll;
+`;
+
+const Wrapper = styled.div`
+flex: 1;
+max-width: 1400px;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+gap: 22px;
+@media (max-width: 600px){
+    gap: 12px;
+    padding: 0px 0px 150px 0px;
+}
+`;
+
+const Card = styled.div`
+height: auto;
+width: 700px;
+padding: 24px;
+border: 1px solid ${({ theme }) => theme.black + 20};
+border-radius: 20px;
+background: linear-gradient(to bottom, rgba(0,0,0,0.9), rgba(0,0,0,0.80));
+box-shadow: 1px 6px 20px 0px ${({ theme }) => theme.black + 15};
+display: flex;
+flex-direction: column;
+margin-top: 1;
+@media (max-width: 768px) {
+  padding: 16px;
+  border-radius: 10px;
+  width: 650px;
+  border-radius: 20px;
+}
+@media (max-width: 450px) {
+  width: 320px;
+  height: auto;
+}
+`;
+
+const Text = styled.div`
+font-weight: 800;
+color: white;
+font-size: 50px;
+padding: 10px;
+text-align: center;
+@media (max-width: 450px) {
+  font-size: 35px;
+}
+`;
+
+const TechLink = styled.div`
+font-weight: 800;
+color: white;
+font-size: 40px;
+padding: 10px 0px;
+transition: all 0.3s ease;
+cursor: pointer;
+transition: all 1s slide-in;
+text-decoration: none;
+&:hover {
+  color: ${({ theme }) => theme.red};
+}
+@media (max-width: 450px) {
+  font-size: 20px;
+}
+`;
+
+const IframeContainer = styled.div`
+width: 100%;
+max-width: 700px;
+height: 400px;
+margin-top: 20px;
+border: 1px solid #000;
+border-radius: 10px;
+overflow: hidden;
+@media (max-width: 450px) {
+  height: 300px;
+}
+`;
+
+const Iframe = styled.iframe`
+width: 100%;
+height: 100%;
+border: none;
+`;
+
+const Wrestling = () => {
+  const [iframeSrc, setIframeSrc] = useState("");
+  const [isVidClicked, setIsVidClicked] = useState(false);
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate('/techniques');
+  }
+  return (
+    <Container>
+      <Wrapper>
+      <Card>
+          <Text>
+            Wrestling Techniques:
+            </Text>
+            {!isVidClicked && (
+              <>  
+            <TechLink onClick={() => {setIframeSrc("https://www.youtube.com/embed/sU4GtV53Ots"); setIsVidClicked(true); }}>1. Double Leg Takedown</TechLink>
+            <TechLink onClick={() => {setIframeSrc("https://www.youtube.com/embed/qmnRo-3Oc9c"); setIsVidClicked(true); }}>2. Single Leg Takedown</TechLink>
+            <TechLink onClick={() => {setIframeSrc("https://www.youtube.com/embed/YuP1wbBQdlY"); setIsVidClicked(true); }}>3. Outside Foot Sweep</TechLink>
+            <TechLink onClick={() => {setIframeSrc("https://www.youtube.com/embed/70ZXpa-nky4"); setIsVidClicked(true); }}>4. Sprawl</TechLink>
+            <TechLink onClick={() => {setIframeSrc("https://www.youtube.com/embed/x4fYB5okQDI"); setIsVidClicked(true); }}>5. Whizzer</TechLink>
+            <TechLink onClick={handleButtonClick}> ← </TechLink>  
+              </>
+            )}
+            {iframeSrc && isVidClicked && (
+              <>
+              <IframeContainer>
+                <Iframe src={iframeSrc} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+              </IframeContainer>
+              <TechLink onClick={() => {setIsVidClicked(false)}}>
+                ←
+              </TechLink>
+              </>
+            )}
+        </Card>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Wrestling
